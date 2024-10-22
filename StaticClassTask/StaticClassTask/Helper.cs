94f497d4fb54fd3b2571faf1ba7e27299a623ded
ayxan217 <ayxanrm-bp217@code.edu.az>
2024-10-22 21:48:26 +0400

    task added

---
+++ StaticClassTask/StaticClassTask/Helper.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticClassTask
+{
+    internal static class Helper
+    {
+     public static  bool IsOdd(int num)
+        {
+            if (num % 2 != 0)
+            {
+                return true;
+            }
+
+            else
+            {
+                return false;
+            }
+        }
+     public static bool IsEven(int num)
+        {
+            if (num % 2 == 0)
+            {
+                return true;
+            }
+
+            else
+            {
+                return false;
+            }
+        }
+     public static bool HasDigit(string text)
+        {
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (Char.IsDigit(text[i]))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static bool CheckPassword(string password)
+        {
+            bool hasLower = false;
+            bool hasUpper = false;
+            bool hasDigit = false;
+
+            for (int i = 0; i < password.Length; i++)
+            {
+                
+                if (Char.IsLower(password[i]))
+                    hasLower = true;
+                if (Char.IsUpper(password[i]))
+                    hasUpper = true;
+                if (Char.IsDigit(password[i]))
+                    hasDigit = true;
+
+                
+                if (hasLower && hasUpper && hasDigit)
+                    return true;
+            }
+            
+
+            return false;
+        }
+
+        public static string Captalize(this string text)
+        {
+            return Char.ToUpper(text[0]) + text.Substring(1).ToLower();
+        }
+
+    }
+}
---
+++ StaticClassTask/StaticClassTask/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace StaticClassTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Helper.IsOdd(51));
+            Console.WriteLine(Helper.IsEven(76));
+            Console.WriteLine(Helper.HasDigit("T539G"));
+            Console.WriteLine(Helper.CheckPassword("fgergA12"));
+            Console.WriteLine(Helper.Captalize("aYxan"));
+        }
+    }
+}
